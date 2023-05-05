19653086d3007f4e463480b496178fc9b94d7e30
hunghg255 <giahunghust@gmail.com>
2023-05-05 14:59:58 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
 import Menu from './Menu/Menu';
+import GitHubCorners from '@uiw/react-github-corners';
 
 function App() {
   return (
     <div className='App'>
       <Menu />
+      <GitHubCorners position='right' href='https://github.com/hunghg255/reactjs-menu-aim' />
+
     </div>
   );
 }
